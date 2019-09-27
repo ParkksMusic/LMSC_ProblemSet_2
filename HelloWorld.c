432a6fe5665677a28b792d37979177d0c84f52ce
Benjamin Parkka <Bparkka@comcast.net>
2019-09-26 21:06:49 -0400

    Problem Set 2
    Hello World Working

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,1 +1,7 @@
-
+#include <stdio.h>
+int main()
+{
+    //Will print out "Hello, World!" in the display
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ ProblemSet2_4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int i = 0;
+    for (int j = 0; j < n; j++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
